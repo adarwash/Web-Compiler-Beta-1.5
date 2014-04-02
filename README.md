ee6e92a4d496d4ce19d20ee3ee9e7d66a7b461aa
ali <adarwash@live.co.uk>
2014-04-02 19:26:40 +0100

    Beta

---
+++ README.md
@@ -0,0 +1,10 @@
+Web-Compiler-Beta-1
+===================
+
+Basic web compiler that only has C/C++.
+
+##To install web compiler 
+
+You will need use the command "/install.sh"
+
+That all to it 
---
+++ install.sh
@@ -0,0 +1,26 @@
+sudo apt-get update && sudo apt-get upgrade
+
+clear
+
+sudo install apt-get install php5
+
+sudo apt-get install build-essential
+
+sudo apt-get install openjdk-7-jdk
+
+apt-cache search jdk
+
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk
+
+export PATH=$PATH:/usr/lib/jvm/java-6-openjdk/bin
+
+sudo apt-get install gcj-jdk
+
+mkdir tmp
+
+chmod 777 tmp
+
+clear
+
+echo “Compiler has been installed”
+
