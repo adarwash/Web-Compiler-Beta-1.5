8459f1b15957d01f8c244e8416fde4cd8be94990
Ali Darwash <alidarwash@MacBook-Pro.local>
2014-10-16 22:15:20 +0100

    Ajax added

--- compile.php
+++ compile.php
@@ -1,128 +1,147 @@
 <?php
 
-$code=$_POST["code"];
-$cargs=$_POST["cargs"];
-$inp=$_POST["inputs"];
-$lang=$_POST["lang"];
-if($lang=="java") $prog_name=$_POST["prog"].'.java'; else if($lang=="C") $prog_name=$_POST["prog"].'_code.c'; else if ($lang=="html") $prog_name=$_POST["prog"].'compile.html'; else if ($lang=="javascript") $prog_name=$_POST["prog"].'compile.html'; else if ($lang=="ruby") $prog_name=$_POST["prog"].'compile.rb'; else if ($lang=="less") $prog_name=$_POST["prog"].'compile.less';
+	$code=$_POST["code"];
+	$cargs=$_POST["cargs"];
+	$inp=$_POST["inputs"];
+	$lang=$_POST["lang"];
+	if($lang=="java") $prog_name=$_POST["prog"].'.java'; else if($lang=="C") $prog_name=$_POST["prog"].'_code.c'; else if ($lang=="html") $prog_name=$_POST["prog"].'compile.html'; else if ($lang=="javascript") $prog_name=$_POST["prog"].'compile.html'; else if ($lang=="ruby") $prog_name=$_POST["prog"].'compile.rb'; else if ($lang=="less") $prog_name=$_POST["prog"].'compile.less';
 
-else $prog_name=$_POST["prog"].'.cpp';
-$prog=$_POST["prog"];
+	else $prog_name=$_POST["prog"].'.cpp';
+	$prog=$_POST["prog"];
 
-$finput=fopen("inputs.tmp","w+");
-fputs($finput,$_POST["inputs"]);
-fclose($finput);
+	$finput=fopen("inputs.tmp","w+");
+	fputs($finput,$_POST["inputs"]);
+	fclose($finput);
 
-$file=fopen($prog_name,"w+");
-fputs($file,stripslashes($code));
-fclose($file);
-if($lang=="C")
-{
-	$output=shell_exec('gcc '.$prog_name.' -o '.$prog.'.out 2>&1');
-	if(is_null($output))
+	$file=fopen($prog_name,"w+");
+	fputs($file,stripslashes($code));
+	fclose($file);
+	if($lang=="C")
 	{
-	$final_out=shell_exec('./'.$prog.'.out '.$cargs.' < inputs.tmp');
-	$outputtext.= $final_out;
+		$output=shell_exec('gcc '.$prog_name.' -o '.$prog.'.out 2>&1');
+		if(is_null($output))
+		{
+		$final_out=shell_exec('./'.$prog.'.out '.$cargs.' < inputs.tmp');
+		$outputtext.= $final_out;
+		}
+		else
+		$outputtext .= "$output";
+		shell_exec('rm '.$prog_name.' '.$prog.'.out inputs.tmp');
 	}
-	else
-	$outputtext .= "$output";
-	shell_exec('rm '.$prog_name.' '.$prog.'.out inputs.tmp');
-}
-if($lang=="C++")
-{
-	$output=shell_exec('g++ '.$prog_name.' -o '.$prog.'.out 2>&1');
-	if(is_null($output))
+	if($lang=="C++")
 	{
-	$final_out=shell_exec('./'.$prog.'.out '.$cargs.' < inputs.tmp');
-	$outputtext.= $final_out;
+		$output=shell_exec('g++ '.$prog_name.' -o '.$prog.'.out 2>&1');
+		if(is_null($output))
+		{
+		$final_out=shell_exec('./'.$prog.'.out '.$cargs.' < inputs.tmp');
+		$outputtext.= $final_out;
+		}
+		else
+		$outputtext .= "$output";
+		shell_exec('rm '.$prog_name.' '.$prog.'out input.tmp');
 	}
-	else
-	$outputtext .= "$output";
-	shell_exec('rm '.$prog_name.' '.$prog.'out input.tmp');
-}
-if($lang=="java")
-{
-	$output=shell_exec('javac '.$prog_name);
-	if(is_null($output))
+	if($lang=="java")
 	{
-	$final_out=shell_exec('java '.$prog.' '.$cargs.' < inputs.tmp');
-	$outputtext.= $final_out;
+		$output=shell_exec('javac '.$prog_name);
+		if(is_null($output))
+		{
+		$final_out=shell_exec('java '.$prog.' '.$cargs.' < inputs.tmp');
+		$outputtext.= $final_out;
+		}
+		else
+		$outputtext .= "$output";
+		shell_exec('rm '.$prog_name.' '.$prog.'.class');
 	}
-	else
-	$outputtext .= "$output";
-	shell_exec('rm '.$prog_name.' '.$prog.'.class');
-}
-if($lang=="php")
-{
-	$output=shell_exec('php-cgi '.$prog_name);
-	if(is_null($output))
+	if($lang=="php")
 	{
-	$final_out=shell_exec('php'.$prog.' '.$cargs.' < inputs.tmp');
-	$outputtext.= $final_out;
+		$output=shell_exec('php-cgi '.$prog_name);
+		if(is_null($output))
+		{
+		$final_out=shell_exec('php'.$prog.' '.$cargs.' < inputs.tmp');
+		$outputtext.= $final_out;
+		}
+		else
+		$outputtext .= "$output";
+		shell_exec('rm '.$prog_name.' '.$prog.'.class');
 	}
-	else
-	$outputtext .= "$output";
-	shell_exec('rm '.$prog_name.' '.$prog.'.class');
-}
-if($lang=="python")
-{
-	$output=shell_exec('python3 '.$prog_name);
-	if(is_null($output))
+	if($lang=="python")
 	{
-	$final_out=shell_exec('py'.$prog.' '.$cargs.' < inputs.tmp');
-	$outputtext.= $final_out;
+		$output=shell_exec('python3 '.$prog_name);
+		if(is_null($output))
+		{
+		$final_out=shell_exec('py'.$prog.' '.$cargs.' < inputs.tmp');
+		$outputtext.= $final_out;
+		}
+		else
+		$outputtext .= "$output";
+		shell_exec('rm '.$prog_name.' '.$prog.'.class');
 	}
-	else
-	$outputtext .= "$output";
-	shell_exec('rm '.$prog_name.' '.$prog.'.class');
-}
-if($lang=="ruby")
-{
-	$output=shell_exec('ruby '.$prog_name);
-	if(is_null($output))
+	if($lang=="ruby")
 	{
-	$final_out=shell_exec('rb'.$prog.' '.$cargs.' < inputs.tmp');
-	$outputtext.= $final_out;
+		$output=shell_exec('ruby '.$prog_name);
+		if(is_null($output))
+		{
+		$final_out=shell_exec('rb'.$prog.' '.$cargs.' < inputs.tmp');
+		$outputtext.= $final_out;
+		}
+		else
+		$outputtext .= "$output";
+		shell_exec('rm '.$prog_name.' '.$prog.'.class');
 	}
-	else
-	$outputtext .= "$output";
-	shell_exec('rm '.$prog_name.' '.$prog.'.class');
-}
-if($lang=="less")
-{
-	$output=shell_exec('lessc '.$prog_name);
-	if(is_null($output))
+	if($lang=="less")
 	{
-	$final_out=shell_exec('less'.$prog.' '.$cargs.' < inputs.tmp');
-	$outputtext.= $final_out;
+		$output=shell_exec('lessc '.$prog_name);
+		if(is_null($output))
+		{
+		$final_out=shell_exec('less'.$prog.' '.$cargs.' < inputs.tmp');
+		$outputtext.= $final_out;
+		}
+		else
+		$outputtext .= "$output";
+		shell_exec('rm '.$prog_name.' '.$prog.'.class');
 	}
-	else
-	$outputtext .= "$output";
-	shell_exec('rm '.$prog_name.' '.$prog.'.class');
-}
-if($lang=="html")
-{
-	$output=shell_exec('html '.$prog_name);
-	if(is_null($output))
+	if($lang=="html")
 	{
-	$final_out=shell_exec('html'.$prog.' '.$cargs.' < inputs.tmp');
-	$outputtext.= $final_out;
+		$output=shell_exec('html '.$prog_name);
+		if(is_null($output))
+		{
+		$final_out=shell_exec('html'.$prog.' '.$cargs.' < inputs.tmp');
+		$outputtext.= $final_out;
+		}
+		else
+		$outputtext .= "$output";
+		//shell_exec('rm '.$prog_name.' '.$prog.'.class');
 	}
-	else
-	$outputtext .= "$output";
-	//shell_exec('rm '.$prog_name.' '.$prog.'.class');
-}
-if($lang=="javascript")
-{
-	$output=shell_exec('javascript '.$prog_name);
-	if(is_null($output))
+	if($lang=="javascript")
 	{
-	$final_out=shell_exec('javascript'.$prog.' '.$cargs.' < inputs.tmp');
-	$outputtext.= $final_out;
+		$output=shell_exec('javascript '.$prog_name);
+		if(is_null($output))
+		{
+		$final_out=shell_exec('javascript'.$prog.' '.$cargs.' < inputs.tmp');
+		$outputtext.= $final_out;
+		}
+		else
+		$outputtext .= "$output";
+		//shell_exec('rm '.$prog_name.' '.$prog.'.class');
 	}
-	else
-	$outputtext .= "$output";
-	//shell_exec('rm '.$prog_name.' '.$prog.'.class');
-}
-include "index.php";
+ 
+	if ($lang =="C"){
+	echo "<textarea>".$outputtext."</textarea>";
+	} else if ($lang =="C++"){ 
+		echo "<textarea>".$outputtext."</textarea>";
+	} else if ($lang =="java"){
+		echo"<textarea>".$outputtext."</textarea>";
+	} else if ($lang =="php"){
+		echo "<textarea>".$outputtext."</textarea>";
+	} else if ($lang =="python"){ 
+		echo "<textarea>".$outputtext."</textarea>";
+	} 	else if ($lang =="html"){
+		echo "<iframe src=".$prog_name." frameborder='0'  style='width: 100%; height: 100%;'></iframe>";
+	} else if ($lang =="javascript"){
+		echo "<iframe src=".$prog_name." frameborder='0'  style='width: 100%; height: 100%;'></iframe>";
+	} else if ($lang =="ruby"){
+		echo "<textarea>".$outputtext."</textarea>";
+	} else if ($lang =="less"){
+		echo "<textarea id='out'>".$outputtext."</textarea>";
+	}
 ?>
--- index.php
+++ index.php
@@ -1,105 +1,114 @@
 <html>
-	<head>
-		<meta charset="utf-8">
+<head>
+	<meta charset="utf-8">
 
-		<!-- CSS links -->
-		<link rel="stylesheet" href="style.css">
-		<link rel="stylesheet" href="editor/lib/codemirror.css">
+	<!-- CSS links -->
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="editor/lib/codemirror.css">
 
-		<!-- Script links -->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-		<script src="editor/lib/codemirror.js"></script>
-		<script src="editor/mode/clike/clike.js"></script>
-		<script src="editor/addon/edit/matchbrackets.js"></script>
+	<!-- Script links -->
+	<script src="https:////ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
-		<script src="editor/lib/codemirror.js"></script>
-<script src="editor/addon/edit/matchbrackets.js"></script>
-<script src="editor/mode/htmlmixed/htmlmixed.js"></script>
-<script src="editor/mode/xml/xml.js"></script>
-<script src="editor/mode/javascript/javascript.js"></script>
-<script src="editor/mode/css/css.js"></script>
-<script src="editor/mode/clike/clike.js"></script>
-<script src="editor/mode/php/php.js"></script>
+	<script src="editor/lib/codemirror.js"></script>
+	<script src="editor/mode/clike/clike.js"></script>
+	<script src="editor/addon/edit/matchbrackets.js"></script>
+	<script src="editor/lib/codemirror.js"></script>
+	<script src="editor/addon/edit/matchbrackets.js"></script>
+	<script src="editor/mode/htmlmixed/htmlmixed.js"></script>
+	<script src="editor/mode/xml/xml.js"></script>
+	<script src="editor/mode/javascript/javascript.js"></script>
+	<script src="editor/mode/css/css.js"></script>
+	<script src="editor/mode/clike/clike.js"></script>
+	<script src="editor/mode/php/php.js"></script>
 
-		<title>ICE-Compiler</title>
+	<title>ICE-Compiler</title>
 
-	</head>
+</head>
 
-	<body>
-		<header>
-			<h1>compiler</h1>
-		</header>
-		
+<body>
+	<header>
+		<h1>compiler</h1>
+	</header>
 
 	<div id="container">
-		<form action="compile.php" method="post">
-		<input type="submit" name="run" id="compileButton" value="Compile & Run" />
+		<form id="coding_area">
+			<input type="submit" name="run" id="compileButton" value="Compile & Run" />
+			<div class="input_boxes">
+				<input type="text" id="project_name" name="prog" id="name"  value="<?php echo $prog ?>" placeholder="Project name">
+				<input type="text" id="stdin" name="inputs" id="stdin" value="<?php echo $inp ?>" placeholder="stdin">
+				<input type="text" id="cargs" name="cargs" id="cargs" value="<?php echo $cargs ?>" placeholder="Command Line Arguments:">
+			</div>
 
 
-		<section id="leftcolumn">
-		<input type="text" name="prog" id="name" size="30" value="<?php echo $prog ?>" placeholder="Project name">
-<textarea id="sourceInput" name="code">
-<?php echo stripslashes($code) ?>
-</textarea>
-		</section>
-
-
-
-	<section id="rightcolumn" >
-		<input type="text" name="inputs" id="stdin" value="<?php echo $inp ?>" placeholder="stdin">
-		<input type="text" name="cargs" id="cargs" size="30" value="<?php echo $cargs ?>" placeholder="Command Line Arguments:">
-		<?php 
-		if ($lang =="C"){
-		echo "<textarea>".$outputtext."</textarea>";
-		} else if ($lang =="C++"){ 
-			echo "<textarea>".$outputtext."</textarea>";
-		} else if ($lang =="java"){
-			echo"<textarea>".$outputtext."</textarea>";
-		} else if ($lang =="php"){
-			echo "<textarea>".$outputtext."</textarea>";
-		} else if ($lang =="python"){ 
-			echo "<textarea>".$outputtext."</textarea>";
-		} 	else if ($lang =="html"){
-			echo "<iframe src=".$prog_name." frameborder='0'  style='width: 100%; height: 100%;'></iframe>";
-		} else if ($lang =="javascript"){
-			echo "<iframe src=".$prog_name." frameborder='0'  style='width: 100%; height: 100%;'></iframe>";
-		} else if ($lang =="ruby"){
-			echo "<textarea>".$outputtext."</textarea>";
-		} else if ($lang =="less"){
-			echo "<textarea>".$outputtext."</textarea>";
-		} else echo "Welcome to ICE Compiler";?>
+			<section id="leftcolumn">
+				<textarea id="sourceInput" name="code">
+					<?php echo stripslashes($code) ?>
+				</textarea>
 			</section>
 
-					<?php $arrayList = array('C','C++','java','php','python','html','javascript','ruby','less');?>
-					<select name="lang" id="languageSelector"> 
-						<?php for($i=0;$i<count($arrayList);$i++)
-						{
-						  if($arrayList[$i]==$_POST['lang'])
-						{
-						  echo '<option selected ="selected" value="'.$arrayList[$i].'">'.$arrayList[$i].'</option>';
-						}
-						  else
-						{
-						  echo '<option value="'.$arrayList[$i].'">'.$arrayList[$i].'</option>';
-						}
-						}
-						?>
-					</select>
+			<section id="rightcolumn" >
 
+				<h2>Welcome</h2>
+			</section>
 
-</form>
-	<script>
-CodeMirror.commands.autocomplete = function(cm) {
-CodeMirror.showHint(cm, CodeMirror.htmlHint);
-}
+			<?php $arrayList = array('C','C++','java','php','python','html','javascript','ruby','less');?>
+			<select name="lang" id="languageSelector"> 
+				<?php for($i=0;$i<count($arrayList);$i++)
+				{
+					if($arrayList[$i]==$_POST['lang'])
+					{
+						echo '<option selected ="selected" value="'.$arrayList[$i].'">'.$arrayList[$i].'</option>';
+					}
+					else
+					{
+						echo '<option value="'.$arrayList[$i].'">'.$arrayList[$i].'</option>';
+					}
+				}
+				?>
+			</select>
+		</form>
+	</div>
+</body>
+</html>
 
-window.editor = CodeMirror.fromTextArea(document.getElementById("sourceInput"), {
-lineNumbers: true,
-matchBrackets: true,
-mode: "application/x-httpd-php",
-indentUnit: 4,
-indentWithTabs: true
-});
-	</script>
+	
+<script>
+	$(document).ready(function(){
+		$('#coding_area').submit(function(){
+
+// show that something is loading
+$('#rightcolumn').html("<b>Processing</b>");
+	$.ajax({
+		type: 'POST',
+		url: 'compile.php', 
+		data: $(this).serialize()
+	})
+	.done(function(data){
+	// show the response
+		$('#rightcolumn').html(data);
+
+		
+	})
+	// just in case posting your form failed
+	.fail(function() {
+		alert( "Posting failed." );
+	});
+	// to prevent refreshing the whole page page
+		return false;
+	});
+	
+	CodeMirror.commands.autocomplete = function(cm) {
+		CodeMirror.showHint(cm, CodeMirror.htmlHint);
+		}
+
+		window.editor = CodeMirror.fromTextArea(document.getElementById("sourceInput"), {
+			lineNumbers: true,
+			matchBrackets: true,
+			mode: "application/x-httpd-php",
+			indentUnit: 6,
+			indentWithTabs: false
+		});
+	});
+</script>
 
 
